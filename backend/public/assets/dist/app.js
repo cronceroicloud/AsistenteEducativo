c9d8b70bf04bed763a3c08492c1e92290da70a91
Carlos Roncero Parra <137909415+cronceroicloud@users.noreply.github.com>
2025-08-16 17:28:31 +0200

    Update app.js

--- backend/public/assets/dist/app.js
+++ backend/public/assets/dist/app.js
@@ -1,4 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+
+//Recupero la variable de entorno con la dirección del backend
+  const NOMBRE  = window.CONFIG?.nombre;
+  const API_URL = window.CONFIG?.apiUrl;
+
+  console.log("CONFIG FRONTs:", window.CONFIG);
+  console.log("API_URLs:", API_URL);
 
 document.addEventListener("DOMContentLoaded", () => {
 
